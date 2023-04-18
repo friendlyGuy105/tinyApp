0820587e8ec424b7ab8efb247ef835e8ed7c2628
friendlyGuy105 <friendlyGuy105@users.noreply.github.com>
2023-04-18 11:28:12 +0000

    just small change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('New Hello World!');
+  res.send('Nice Hello World!');
 });
 
 app.listen(PORT, () => {
